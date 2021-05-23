5234bc6e45942cd02244d1810a8501c1148d19aa
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-23 16:21:32 -0600

    added zookeeper json and routes, ran tests

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
+
 router.use(animalRoutes);
+router.use(require ('./zookeeperRoutes'));
 
 module.exports = router;
