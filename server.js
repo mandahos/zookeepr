3dcaa49a52b300a746998d240d7740e91421a635
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-23 16:52:42 -0600

    verified code, missing one line, added some notes

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
-
 
 const PORT = process.env.PORT || 3008;
 const app = express();
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+
+
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 //merges the paths of all public folders to include styling features
 app.use(express.static('public'));
+
+//Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
